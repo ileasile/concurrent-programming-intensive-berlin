0cc7ef676b0e2bfd984019016abc68c2d4461f73
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2023-07-05 11:37:49 +0200

    FAA-based queue simplified

--- src/day2/FAABasedQueueSimplified.kt
+++ src/day2/FAABasedQueueSimplified.kt
@@ -9,26 +9,29 @@ class FAABasedQueueSimplified<E> : Queue<E> {
     private val deqIdx = atomic(0)
 
     override fun enqueue(element: E) {
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = enqIdx.value
-        enqIdx.value = i + 1
-        // TODO: Atomically install the element into the cell
-        // TODO: if the cell is not poisoned.
-        infiniteArray[i].value = element
+        while (true) {
+            val i = enqIdx.getAndIncrement()
+            if (infiniteArray[i].compareAndSet(null, element)) return
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun dequeue(): E? {
-        // Is this queue empty?
-        if (enqIdx.value <= deqIdx.value) return null
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = deqIdx.value
-        deqIdx.value = i + 1
-        // TODO: Try to retrieve an element if the cell contains an
-        // TODO: element, poisoning the cell if it is empty.
-        return infiniteArray[i].value as E
+        while (true) {
+            var enqI = enqIdx.value
+            var deqI = deqIdx.value
+            while (true) {
+                if (enqIdx.value == enqI) break;
+                enqI = enqIdx.value
+                deqI = deqIdx.value
+            }
+            if (enqI <= deqI) return null
+
+            val i = deqIdx.getAndIncrement()
+
+            if (infiniteArray[i].compareAndSet(null, POISONED)) continue
+            return infiniteArray[i].value as E
+        }
     }
 }
 
